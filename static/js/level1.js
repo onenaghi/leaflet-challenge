317b0c544fb771780b02a41b287acdc0805262d6
onenaghi <onenaghi@yahoo.com>
2020-09-10 23:49:46 -0500

    add map create object

--- static/js/level1.js
+++ static/js/level1.js
@@ -1,1 +1,7 @@
 // code for creating Basic Map (Level 1)
+// code for creating Basic Map (Level 1)
+// Creating map object
+const myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 3
+});
